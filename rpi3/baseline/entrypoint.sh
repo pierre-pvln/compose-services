2e7d437c9d4fc8146cd7302c5b93ad1e30ebc7fa
Pierre Veelen <pierre@pvln.nl>
2018-06-14 17:18:45 +0200

    Update entrypoint.sh

--- rpi3/baseline/entrypoint.sh
+++ rpi3/baseline/entrypoint.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
+# ==========================================
+# Raspberry Pi 3 Baseline entrypoint.sh 
+# ==========================================
 
+# This script is started (in the container) when the container is started
+#
+
+echo "Running baseline entrypoint.sh" 
