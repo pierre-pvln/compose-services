87b1244ad99797a363f4bc090ab1aae24775f6ed
Pierre Veelen <pierre@pvln.nl>
2018-06-15 13:06:37 +0200

    Update entrypoint.sh

--- rpi3/voluntas/entrypoint.sh
+++ rpi3/voluntas/entrypoint.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
+# ==========================================
+# Raspberry Pi 3 Voluntas entrypoint.sh 
+# ==========================================
+
+# This script is started (in the container) when the container is started
+#
+
+echo "Running Voluntas entrypoint.sh" 
 
